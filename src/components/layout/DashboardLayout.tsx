c54ba56b2e17c991381f49599df817d48481bfe1
Winifred <debpuurwinifred@gmail.com>
2025-12-15 07:15:43 +0000

    Add z-50 to sidebar wrapper to be above overlay

--- src/components/layout/DashboardLayout.tsx
+++ src/components/layout/DashboardLayout.tsx
@@ -32,7 +32,7 @@ export const DashboardLayout: React.FC = () => {
 
             {/* Sidebar */}
             <div
-                className={`${isMobileSidebarOpen ? 'translate-x-0' : '-translate-x-full'} lg:translate-x-0 transition-transform duration-300`}
+                className={`${isMobileSidebarOpen ? 'translate-x-0' : '-translate-x-full'} lg:translate-x-0 transition-transform duration-300 z-50`}
                 onClick={(e) => e.stopPropagation()}
             >
                 <Sidebar
